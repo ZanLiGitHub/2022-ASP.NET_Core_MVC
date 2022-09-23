3a3fee0a13701758da0d41c85332f835af9d0b4a
Zan Li <zanli0718@gmail.com>
2022-09-23 22:38:53 +0930

    1

--- ASP.NET_Core_MVC/ASP.NET_Core_MVC/Data/Enum/RaceCategory.cs
+++
@@ -1,9 +0,0 @@
-namespace ASP.NET_Core_MVC.Data.Enum;
-
-public enum RaceCategory
-{
-    Marthon,
-    Ultra,
-    FiveK,
-    HalfMarathon
-}
---
+++ BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    // 数据库中的表名
+    public class Category
+    {
+        // 数据库中的列名
+        // [Key]表示主键
+        [Key] public int Id { get; set; }
+        // [Required]表示必要项
+        [Required]public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        // 用 = 设置默认值
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+        
+
+    }
+}
