768d813391612e63a7f0c2c0663812a3b769d540
Zan Li <zanli0718@gmail.com>
2022-09-25 05:58:10 +0930

    3

--- BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBook/BulkyBookWeb/Controllers/CategoryController.cs
@@ -25,14 +25,104 @@ namespace BulkyBookWeb.Controllers
             return View();
         }
         
-        
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
+            //自定义error
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            //查看输入是否合法
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index");
+            }
+            //输入不合法的话不返回上一页
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int?id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //从数据库寻找id
+            var categoryFromDb = _db.Categories.Find(id); 
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            //自定义error
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            //查看输入是否合法
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            //输入不合法的话不返回上一页
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            //从数据库寻找id
+            var categoryFromDb = _db.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var obj = _db.Categories.Find(id);
+            //查看输入是否合法
+            if (obj == null)
+            { 
+                return NotFound();
+            }
+
+            _db.Categories.Remove(obj);
             _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
             return RedirectToAction("Index");
         }
     }
--- BulkyBook/BulkyBookWeb/Models/Category.cs
+++ BulkyBook/BulkyBookWeb/Models/Category.cs
@@ -9,8 +9,9 @@ namespace BulkyBookWeb.Models
         // [Key]表示主键
         [Key] public int Id { get; set; }
         // [Required]表示必要项
-        [Required]public string Name { get; set; }
-        public int DisplayOrder { get; set; }
+        [Required]public string? Name { get; set; }
+        // [Range(X,Y]表示范围
+        [Range(1,100)]public int DisplayOrder { get; set; }
         // 用 = 设置默认值
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
         
